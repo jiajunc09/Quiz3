af74dc22df7626f2fc4db025680a8622a2f5dae7
Cheang Jia Jun <61217047+jiajunc09@users.noreply.github.com>
2020-07-09 15:45:28 +0800

    Create Animal.java

---
+++ Animal.java
@@ -0,0 +1,10 @@
+package quiz3;
+
+public class Animal{
+    public static int age;
+    
+    public void introduceYourself(){
+        System.out.printf("Morr. I am a animal.");
+        System.out.println("and I am" + this.age + "years old.");
+    }
+}
